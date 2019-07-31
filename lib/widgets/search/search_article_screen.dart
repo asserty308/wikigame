a4e94e4eef8c49c562c76792a7d08b4415fe6c43
asserty308 <yannickassert@gmail.com>
2019-07-31 19:30:07 +0200

    showing search right after layout

--- lib/widgets/search/search_article_screen.dart
+++ lib/widgets/search/search_article_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wikigame/api/wiki_api.dart';
-import 'package:wikigame/api/wiki_article.dart';
-import 'package:wikigame/style/text_styles.dart';
+import 'package:wikigame/widgets/search/search_delegate.dart';
 
 class SearchArticleScreen extends StatefulWidget {
   @override
@@ -9,6 +7,14 @@ class SearchArticleScreen extends StatefulWidget {
 }
 
 class SearchArticleScreenState extends State<SearchArticleScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // run 'afterFirstlayout' after first build()
+    WidgetsBinding.instance.addPostFrameCallback((_) => afterFirstlayout(context));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,80 +34,12 @@ class SearchArticleScreenState extends State<SearchArticleScreen> {
       ),
     );
   }
-}
 
-class ArticleSearchDelegate extends SearchDelegate {
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: Icon(Icons.clear),
-        onPressed: () {
-          query = '';
-        },
-      )
-    ];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      icon: Icon(Icons.arrow_back),
-      onPressed: () {
-        close(context, null);
-      },
+  void afterFirstlayout(BuildContext context) {
+    // show search when showing screen
+    showSearch(
+      context: context,
+      delegate: ArticleSearchDelegate(),
     );
   }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    return showSearchResults();
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    return showSearchResults();
-  }
-
-  Widget showSearchResults() {
-    var articles = searchArticles(query);
-
-    return FutureBuilder<List<WikiArticle>>(
-      future: articles,
-      builder: (BuildContext context, AsyncSnapshot<List<WikiArticle>> snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          var widgetList = <Widget>[];
-
-          if (snapshot.data == null) {
-            return Center(child: Text('Keine Artikel gefunden'));
-          }
-
-          for (var article in snapshot.data) {
-            widgetList.add(
-              ListTile(
-                title: HeaderText(text: article.title),
-                onTap: () {
-                  Navigator.pop(context, article); // pop search delegate
-                  Navigator.pop(context, article); // pop screen
-                },
-              )
-            );
-          }
-
-          return ListView.builder(
-            itemCount: widgetList.length,
-            itemBuilder: (context, index) {
-              return widgetList[index];
-            }
-          );
-        }
-
-        // show progress indicator when search results aren't available
-        return Center(
-          child: CircularProgressIndicator(),
-        );
-      },
-    );
-  }
-
 }
