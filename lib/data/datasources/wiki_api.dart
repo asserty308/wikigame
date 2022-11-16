6f5025cd8d7d9c3991cfc0e49e3740efd58d6e75
asserty308 <yannickassert@gmail.com>
2022-11-16 21:38:08 +0100

    api improvements

--- lib/data/datasources/wiki_api.dart
+++ lib/data/datasources/wiki_api.dart
@@ -3,9 +3,11 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:wikigame/data/models/wiki_article.dart';
 
+class PageNotFoundException implements Exception {}
+
 class WikiApi {
   /// The base url to query the wikipedia api
-  final _baseUrl = 'de.wikipedia.org';
+  final _baseUrl = 'en.wikipedia.org';
 
   Uri _buildUrl([Map<String, dynamic>? query]) => Uri.https(_baseUrl, 'w/api.php', query);
 
@@ -17,6 +19,25 @@ class WikiApi {
 
   /// Calls the Wikipedia API for random articles.
   /// Returns the given amount of articles.
+  /// 
+  /// https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnlimit=2&rnnamespace=0
+  /// 
+  /// {
+  ///   "query":{
+  ///     "random":[
+  ///       {
+  ///         "id":123,
+  ///         "ns":0,
+  ///         "title":"ABC"
+  ///       },
+  ///       {
+  ///         "id":456,
+  ///         "ns":0,
+  ///         "title":"DEF"
+  ///       }
+  ///     ]
+  ///   }
+  /// }
   Future<List<WikiArticle>> getRandomArticles(int amount) async {
     final url = _buildUrl({
       'action': 'query',
@@ -30,11 +51,25 @@ class WikiApi {
     final randomArticles = response['query']['random'] as List;
 
     return randomArticles
-      .map((item) => WikiArticle.fromJSON(item as Map<String, dynamic>))
+      .map((item) => WikiArticle.fromJson(item as Map<String, dynamic>))
       .toList();
   }
 
   /// Fetches the id of the article by a given title
+  /// 
+  /// https://en.wikipedia.org/w/api.php?action=query&format=json&titles=Flutter_(software)&formatversion=2
+  /// 
+  /// {
+  ///   "query": {
+  ///     "pages":[
+  ///       {
+  ///         "pageid":54699721,
+  ///         "ns":0,
+  ///         "title":"Flutter (software)"
+  ///       }
+  ///     ]
+  ///   }
+  /// }
   Future<int> getIdFromTitle(String title) async {
     final url = _buildUrl({
       'action': 'query',
@@ -44,16 +79,21 @@ class WikiApi {
     });
 
     final response = await _getJson(url);
-    return response['query']['pages'][0]['pageid'] as int;
+    final pages = response['query']['pages'] as List;
+    
+    if (pages.isEmpty) {
+      throw PageNotFoundException();
+    }
+
+    final article = WikiArticle.fromJson(pages.first as Map<String, dynamic>);
+    return article.id;
   }
 
   /// Fetches the summary for the article with the given id
-  Future<String> getArticleSummary(int id) async {
-    // The url to fetch the summary.
-    // formatversion=2 makes sure, that the response pages are returned as json array.
-    // This is needed because the id of an article could not be valid anymore because of
-    // a redirect. (example on german wiki: Filmindustrie (1566124) became Filmwirtschaft (202198))
-    final uri = _buildUrl({
+  /// 
+  /// https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro&explaintext&redirects=1&pageids=54699721&formatversion=2
+  Future<String> getArticleExtract(int id) async {
+    final url = _buildUrl({
       'action': 'query',
       'format': 'json',
       'prop': 'extracts&exintro&explaintext',
@@ -62,17 +102,25 @@ class WikiApi {
       'formatversion': 2,
     });
 
-    final response = await _getJson(uri);
+    final response = await _getJson(url);
 
     // get page object
     final pages = response['query']['pages'] as List;
-    final page = WikiArticle.fromJSON(pages.first as Map<String, dynamic>, idKey: 'pageid');
+
+    if (pages.isEmpty) {
+      throw PageNotFoundException();
+    }
+
+    final page = WikiArticle.fromJson(pages.first as Map<String, dynamic>);
 
     return page.extract ?? 'Summary not available.';
   }
 
+  /// Returns the url of the image associated to the article with [id].
+  /// 
+  /// https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages&piprop=original&redirects=1&pageids=162510&formatversion=2
   Future<String?> getArticleImageUrl(int id) async {
-    final uri = _buildUrl({
+    final url = _buildUrl({
       'action': 'query',
       'format': 'json',
       'prop': 'pageimages',
@@ -82,16 +130,24 @@ class WikiApi {
       'formatversion': 2,
     });
 
-    final response = await _getJson(uri);
+    final response = await _getJson(url);
 
-    final pageObj = response['query']['pages'][0];
-    return pageObj['original']['source'] as String?;
+    final pages = response['query']['pages'] as List;
+
+    if (pages.isEmpty) {
+      throw PageNotFoundException();
+    }
+
+    final page = pages.first;
+    return page['original']['source'] as String?;
   }
 
   /// Fetches all links in the article which lead to other wikipedia articles.
   /// The links are returned as a list of the titles of the article.
+  /// 
+  /// https://en.wikipedia.org/w/api.php?action=query&format=json&prop=links&pllimit=max&titles=Flutter_(software)&formatversion=2
   Future<List<String>> getArticleLinksByTitle(String title) async {
-    final uri = _buildUrl({
+    final url = _buildUrl({
       'action': 'query',
       'format': 'json',
       'prop': 'links',
@@ -100,14 +156,19 @@ class WikiApi {
       'formatversion': 2,
     });
 
-    var response = await _getJson(uri);
+    var response = await _getJson(url);
 
     final links = <String>[];
 
     while (true) {
       // parse page object
-      final pageObj = response['query']['pages'][0];
-      final pageLinks = pageObj['links'];
+      final pages = response['query']['pages'] as List;
+
+      if (pages.isEmpty) {
+        throw PageNotFoundException();
+      }
+
+      final pageLinks = pages.first['links'] as List;
 
       // fetch all links from response and add them to the link list
       for (final linkObj in pageLinks) {
@@ -129,16 +190,18 @@ class WikiApi {
 
       // start new request
       final contParam = response['continue']['plcontinue'];
-      uri.queryParameters.addAll({'plcontinue': contParam});
-      response = await _getJson(uri);
+      url.queryParameters['plcontinue'] = contParam;
+      response = await _getJson(url);
     }
 
     return links;
   }
 
   /// Fetches an article by a given category
-  Future<List<String>> getArticlesWithCategory(String category) async {
-    final uri = _buildUrl({
+  /// 
+  /// https://en.wikipedia.org/w/api.php?action=query&format=json&cmnamespace=0&list=categorymembers&cmtitle=Category:Space
+  Future<List<dynamic>> getArticlesWithCategory(String category) async {
+    final url = _buildUrl({
       'action': 'query',
       'format': 'json',
       'cmnamespace': 0,
@@ -146,24 +209,26 @@ class WikiApi {
       'cmtitle': 'Category:$category',
     });
 
-    final response = await _getJson(uri);
-    return response['query']['pages'][0]['pageid'] as List<String>;
+    final response = await _getJson(url);
+    return response['query']['categorymembers'] as List;
   }
 
   /// Perform a search for titles containing the searchTerm
+  /// 
+  /// https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=Flutter
   Future<List<WikiArticle>> searchArticles(String searchTerm) async {
-    final uri = _buildUrl({
+    final url = _buildUrl({
       'action': 'query',
       'format': 'json',
       'list': 'search',
       'srsearch': searchTerm,
     });
     
-    final response = await _getJson(uri);
+    final response = await _getJson(url);
     final results = response['query']['search'] as List;
 
     return results
-      .map((e) => WikiArticle.fromJSON(e as Map<String, dynamic>, idKey: 'pageid'))
+      .map((e) => WikiArticle.fromJson(e as Map<String, dynamic>))
       .toList();
   }
 }
